4fd3819d960bfb74ffc1c397d728aa36d7a105e9
gcode101 <gcode101@users.noreply.github.com>
2024-03-28 15:26:20 -0400

    updated index.js

--- Server/index.js
+++ Server/index.js
@@ -11,20 +11,13 @@ const { createCard, getCard, getShowCard, updateCard, deleteCard } = require("./
 
 const app = express();
 app.use(cors({
-	origin: ["https://digitalbusinesscard2024.vercel.app"],
+	origin: ["*"],
 	methods: ["GET", "POST", "PUT", "DELETE"],
 	credentials: true
 }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://digitalbusinesscard2024.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
 app.use(express.json());
-app.use(express.static('public'));	
+app.use(express.static('public'));
 app.use(cookieParser());
 
 mongoose.connect(dburl);
