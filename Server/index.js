e8fd69ef2d7149fb9323fc0fda449d6db825dae7
gcode101 <gcode101@users.noreply.github.com>
2024-03-28 13:29:50 -0400

    modified server side to allow cors access control

--- Server/index.js
+++ Server/index.js
@@ -17,6 +17,14 @@ app.use(cors({
 	methods: ["GET", "POST", "PUT", "DELETE"],
 	credentials: true
 }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://digitalbusinesscard2024.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(cookieParser());
 
 mongoose.connect(dburl);
