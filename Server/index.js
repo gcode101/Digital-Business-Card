b08fa69f3d12d508b960676d7effbdda318f3c9a
gcode101 <gcode101@users.noreply.github.com>
2024-03-28 14:03:21 -0400

    updated profile

--- Server/index.js
+++ Server/index.js
@@ -17,7 +17,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://digitalbusinesscard2024.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
