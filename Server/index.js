fa10778039eaea34eb394a1d4c89d89c734a2b3a
gcode101 <gcode101@users.noreply.github.com>
2024-03-28 13:55:42 -0400

    updated profile

--- Server/index.js
+++ Server/index.js
@@ -17,14 +17,14 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://digitalbusinesscard2024.vercel.app');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('public'));	
 app.use(cookieParser());
 
 mongoose.connect(dburl);
